0100acead7f1f1c74b78af8d83b5950bbc86317c
VinjitR <regulagedda.1999@gmail.com>
2020-06-21 19:58:21 +0530

    Combining Reducers

--- react/src/components/ContactComponent.js
+++ react/src/components/ContactComponent.js
@@ -3,6 +3,13 @@ import { Breadcrumb, BreadcrumbItem, Button, Label, Col,Row} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
 
+const required = (val)=> val && val.length;
+const maxLength = (len)=> (val)=> !(val) ||(val.length <= len);
+const minLength = (len)=>(val)=> (val)&&(val.length >= len);
+const isNumber= (val)=>!isNaN(Number(val));
+const validEmail =(val) =>/^[A-z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
+
 class Contact extends Component
 {
   constructor(props) {
@@ -70,7 +77,16 @@ handleSubmit(values) {
                                     <Control.text model=".firstname" id="firstname" name="firstname"
                                         placeholder="First Name"
                                         className="form-control"
+                                        validators={{required,minLength: minLength(3),maxLength:maxLength(15)}}
                                          />
+                                    <Errors
+                                        className='text-danger'
+                                        model=".firstname"
+                                        show='touched'
+                                        messages={{required:'Required',
+                                          minLength:'Must be > than 2 character',
+                                          maxLength: 'Must be < than 15'}}
+                                    />
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -79,7 +95,17 @@ handleSubmit(values) {
                                     <Control.text model=".lastname" id="lastname" name="lastname"
                                         placeholder="Last Name"
                                         className="form-control"
+                                        validators={{required,minLength: minLength(3),maxLength:maxLength(15)}}
                                          />
+                                    <Errors
+                                        className='text-danger'
+                                        model=".lastname"
+                                        show='touched'
+                                        messages={{required:'Required',
+                                          minLength:'Must be > than 2 character',
+                                          maxLength: 'Must be < than 15'}}
+                                    />
+
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -88,15 +114,35 @@ handleSubmit(values) {
                                     <Control.text model=".telnum" id="telnum" name="telnum"
                                         placeholder="Tel. Number"
                                         className="form-control"
+                                        validators={{required,minLength: minLength(3),maxLength:maxLength(15), isNumber}}
                                          />
+                                    <Errors
+                                        className='text-danger'
+                                        model=".telnum"
+                                        show='touched'
+                                        messages={{required:'Required',
+                                          minLength:'Must be > than 2 character',
+                                          maxLength: 'Must be < than 15',
+                                          isNumber:'Should be in numbers'}}
+                                    />
                                 </Col>
+
                             </Row>
                             <Row className="form-group">
                                 <Label htmlFor="email" md={2}>Email</Label>
                                 <Col md={10}>
                                     <Control.text model=".email" id="email" name="email"
                                         placeholder="Email"
-                                        className="form-control" />
+                                        className="form-control"
+                                        validators={{required,validEmail}}
+                                        />
+                                    <Errors
+                                        className='text-danger'
+                                        model=".email"
+                                        show='touched'
+                                        messages={{required:'Required',
+                                         validEmail:'Invalid email address'}}
+                                    />    
                                 </Col>
                             </Row>
                             <Row className="form-group">
--- react/src/components/DishdetailComponent.js
+++ react/src/components/DishdetailComponent.js
@@ -1,8 +1,93 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import '../App.css';
 import '../index.css';
-import { Card, CardImg, CardImgOverlay, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem,Button,Label, Modal, ModalHeader,ModalBody,Row, Col } from 'reactstrap';
 import { Link} from 'react-router-dom';
+import { Control, LocalForm, Errors} from 'react-redux-form';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleComments = this.handleComments.bind(this);
+    }
+
+
+    toggleModal() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleComments(values) {
+
+        this.toggleModal();
+
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+    }
+
+    render() {
+        return(
+            <div className='mt-auto'>
+                <Button outline onClick={this.toggleModal}><span className="fa fa-edit fa-lg"></span>Submit Comment</Button>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Submit Comments</ModalHeader>
+            <ModalBody>
+                <LocalForm onSubmit={(values) => this.handleComments(values)}>
+                    <Row  className="form-group">
+                        <Label for="rating" md={12}>Rating</Label>
+                        <Col  md={12}>
+                            <Control.select model=".rating" id="rating" name="rating" className="form-control" >
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Control.select>
+                        </Col>
+                    </Row>
+                    <Row className="form-group">
+                        <Label htmlFor="author"  md={12}>Your Name</Label>
+                        <Col  md={12}>
+                            <Control.text model=".author"
+                              id="author" name="author" placeholder="Author" className="form-control"
+                              validators={{ required, minLength: minLength(2), maxLength: maxLength(15) }} />
+                            <Errors className="text-danger" model=".author" show="touched"
+                              messages={{ required: 'Required',
+                                 minLength: 'Must be greater than 2 characters',
+                                 maxLength: 'Must be 15 charaters or less' }} />
+                        </Col>
+                    </Row>
+
+                    <Row className="form-group">
+                        <Label htmlFor="feedback"  md={12}>Your feedback</Label>
+                        <Col  md={12}>
+                            <Control.textarea model=".comment" id="comment" name="comment"
+                             resize="none" rows="6" className="form-control" validators={{ required }} />
+                            <Errors className="text-danger" model=".comment" show="touched" messages={{ required: 'Required' }} />
+                        </Col>
+                    </Row>
+
+                    <Button type="submit" value="submit" color="primary">Submit</Button>
+                </LocalForm>
+            </ModalBody>
+        </Modal>
+
+            </div>
+        )
+    }
+}
+
 
    function RenderComm({comments}){
         if(comments==null)
@@ -32,13 +117,16 @@ import { Link} from 'react-router-dom';
                                             Comments
                                         </h4><br/>
                                         {cmts}
+                                        <CommentForm >
+
+                                        </CommentForm>
                 
                                     </ul>
 
         )
     }
     
-  
+    
     function RenderDish({dish})
       {
         if(dish!=null){
